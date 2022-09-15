6423e01f37a767f598253d97da7fcde52586636e
Matthew Kim <matthew.kim.733@my.csun.edu>
2022-09-15 15:56:42 -0700

    added a card wrapper

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
     <div>
       <h2>Let's get started!</h2>
       <p>This is also visible! </p>
-      <Expenses items={expenses}/>
+      <Expenses items={expenses} />
     </div>
   );
 }
